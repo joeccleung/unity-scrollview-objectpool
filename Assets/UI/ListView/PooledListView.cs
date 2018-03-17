899ad71688136cee46da52981f3e153ada031379
joeccleung <joeccleung@gmail.com>
2018-03-17 10:31:20 -0700

    Provide View Port RectTransform and Item Height field on PooledListView

--- Assets/UI/ListView/PooledListView.cs
+++ Assets/UI/ListView/PooledListView.cs
@@ -6,18 +6,33 @@ using UnityEngine.UI;
 public class PooledListView : MonoBehaviour
 {
     [SerializeField] ScrollRect ScrollRect;
+    [SerializeField] RectTransform viewPortT;
     [SerializeField] RectTransform DragDetectionT;
     [SerializeField] RectTransform ContentT;
+    [SerializeField] float ItemHeight = 1;      // TODO: Replace it with dynamic height
     [SerializeField] int BufferSize;
     [SerializeField] ListViewItemPool ItemPool;
 
+    public int TargetVisibleItemCount { get { return Mathf.CeilToInt(viewPortT.sizeDelta.y / ItemHeight); } }
+
     int[] data;
+    int dataHead = 0;
+    int dataTail = 0;
 
     public void Setup(int[] data)
     {
         this.data = data;
 
-        DragDetectionT.sizeDelta = new Vector2(DragDetectionT.sizeDelta.x, this.data.Length * 100);
+        DragDetectionT.sizeDelta = new Vector2(DragDetectionT.sizeDelta.x, this.data.Length * ItemHeight);
 
+        for(int i = 0; i < TargetVisibleItemCount + BufferSize; i++)
+        {
+            GameObject itemGO = ItemPool.ItemBorrow();
+            itemGO.transform.SetParent(ContentT);
+            itemGO.SetActive(true);
+            itemGO.transform.localScale = Vector3.one;
+            itemGO.GetComponent<ListViewItem>().Setup(data[dataTail]);
+            dataTail++;
+        }
     }
 }
