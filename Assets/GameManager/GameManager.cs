d5b7202f9fb3af827da8d8ceca998fffc5d8a3a0
joeccleung <joeccleung@gmail.com>
2018-03-17 10:04:35 -0700

    GameManager object to inflate list view with demo data

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] PooledListView listView;
+    [SerializeField] int DataCount;
+
+    void Start()
+    {
+        int[] demoData = new int[DataCount];
+        for(int i = 0; i < DataCount; i++)
+        {
+            demoData[i] = i + 1;
+        }
+
+        listView.Setup(demoData);
+    }
+}
