6712681d73803aaf5c8b75d2f5daf79226135133
joeccleung <joeccleung@gmail.com>
2018-03-17 10:03:46 -0700

    Rename ListViewPage to PooledListView

---
+++ Assets/UI/ListView/PooledListView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PooledListView : MonoBehaviour
+{
+    [SerializeField] ScrollRect ScrollRect;
+    [SerializeField] RectTransform DragDetectionT;
+    [SerializeField] RectTransform ContentT;
+    [SerializeField] int BufferSize;
+    [SerializeField] ListViewItemPool ItemPool;
+
+    int[] data;
+
+    public void Setup(int[] data)
+    {
+        this.data = data;
+
+        DragDetectionT.sizeDelta = new Vector2(DragDetectionT.sizeDelta.x, this.data.Length * 100);
+
+    }
+}
